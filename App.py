41fb72457920f0dc90e1051838939fe626dbb81f
Denis Muniz Silva <denis.m.s.777@hotmail.com>
2026-03-02 23:25:22 -0300

    added

--- App.py
+++ App.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 
 pages = {
-    "Receitas": [
+    "➡️ Receitas": [
         st.Page("pages/rcl.py", title="Receita Corrente Geral"),
         st.Page("pages/Transferencias.py", title="Transferências Correntes"),
         st.Page("pages/Tributario.py", title="Tributação"),
         st.Page("pages/Previsao_Receitas.py", title="Previsão de Receitas"),
         st.Page("pages/Fontes_de_Dados_rcl.py", title="Fontes de Dados"),
     ],
-    "Outros": [
+    "➡️ Outros": [
         st.Page("pages/Folha.py", title="Folha de Pagamento"),
         st.Page("pages/Fontes_de_Dados_Folha.py", title="Fontes de Dados"),
     ],
